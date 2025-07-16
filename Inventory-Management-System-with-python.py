1ae4018a33fcaa4294e7aecaf18f17093a58a005
Ali Najafi <76534541+Ali9721@users.noreply.github.com>
2025-07-16 09:50:55 -0700

    Inventory-Management-System-with-python
    1. At first, using "csv"
    
    2. Add item
    
    3. view items
    
    4. Main Menu

---
+++ Inventory-Management-System-with-python.py
@@ -0,0 +1,48 @@
+# At first, using "csv"
+import csv
+
+FILENAME = 'inventory.csv'
+
+# Add item
+def add_item():
+    name = input("Please enter item name: ")
+    quantity = int(input("Please enter quantity: "))
+    price = float(input("Please enter price: "))
+
+    with open(FILENAME, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([name, quantity, price])
+    print("Item added!")
+
+# view items
+def view_items():
+    try:
+        with open(FILENAME,'r') as file:
+            reader = csv.reader(file)
+            print(f"{'Name': <20} {'Quantity': <10} {'Price': <10}")
+            print("_"*40)
+            for row in reader:
+                print(f"{row[0]:<20} {row[1]:<10} {row[2]:<10}")
+    except FileNotFoundError:
+        print("No Inventory Found!")
+
+# Main Menu
+def main():
+    while True:
+        print("\nInventory Management System")
+        print("1. Add Item")
+        print("2. View Items")
+        print("3. Exit")
+        choice = input("choose an option: ")
+        if choice == "1":
+            add_item()
+        elif choice == '2':
+            view_items()
+        elif choice == '3':
+            print("goodbye!")
+            break
+        else:
+            print( "Invalid choice.")
+
+if __name__ == "__main__":
+    main()
